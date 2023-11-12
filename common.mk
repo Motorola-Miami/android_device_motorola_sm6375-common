d6df33a295df3186dfec74f6410df25a8028e8a9
danascape <danascape@gmail.com>
2023-11-12 10:29:24 +0100

    sm6375-common: Drop atrace HAL
    It is deprecated in Android 14.
    
    Change-Id: I5af8056853058ad29be7debb5c5af5dea83d84a9

--- common.mk
+++ common.mk
@@ -79,10 +79,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
