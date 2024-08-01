a609bc0a42dd24b06f8abc5882373e8f47e99ca5
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-08-01 10:47:25 +0000

    sm6375-common: Add `Dolby Atmos`
    repo - https://github.com/userariii/vendor_motorola-dolby.git
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- common.mk
+++ common.mk
@@ -117,6 +117,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Dolby
+$(call inherit-product, hardware/motorola/dolby/setup.mk)
+
 # Authsecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
