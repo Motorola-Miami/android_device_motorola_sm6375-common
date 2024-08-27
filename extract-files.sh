3dd6181e6afa0048c041d93812d5162af70a628b
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    sm6375-common: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -14,10 +13,12 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-export TARGET_ENABLE_CHECKELF=false
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [[ ! -f "${HELPER}" ]]; then
+if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
@@ -27,16 +28,20 @@ source "${HELPER}"
 CLEAN_VENDOR=true
 
 ONLY_COMMON=
+ONLY_FIRMWARE=
 ONLY_TARGET=
 KANG=
 SECTION=
 
-while [[ "${#}" -gt 0 ]]; do
+while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        -oc | --only-common)
+        --only-common)
             ONLY_COMMON=true
             ;;
-        -ot | --only-target)
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        --only-target)
             ONLY_TARGET=true
             ;;
         -n | --no-cleanup)
@@ -57,52 +62,84 @@ while [[ "${#}" -gt 0 ]]; do
     shift
 done
 
-if [[ -z "${SRC}" ]]; then
+if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib*/libwfdmmsrc_system.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
         system_ext/lib64/libwfdnative.so)
-            ${PATCHELF} --remove-needed "android.hidl.base@1.0.so" "${2}"
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
         system_ext/etc/permissions/moto-telephony.xml)
+            [ "$2" = "" ] && return 0
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
         system_ext/priv-app/ims/ims.apk)
+            [ "$2" = "" ] && return 0
             apktool_patch "${2}" "$MY_DIR/ims-patches"
             ;;
         vendor/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml)
+            [ "$2" = "" ] && return 0
             sed -ni '/default.*fqname/!p' "${2}"
+            ;;
+        vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
+        *)
+            return 1
             ;;
         vendor/etc/qcril_database/qcrilNr.db|\
         vendor/etc/qcril_database/upgrade/config/*)
             sed -i '/persist.vendor.radio.poweron_opt/ s/1/0/g' "${2}"
             ;;
-        vendor/lib64/libwvhidl.so)
-            [[ -z "${2}" ]] && return 0
-            grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
-            ;;
     esac
+    return 0
+}
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
-if [[ -z "${ONLY_TARGET}" ]]; then
+function prepare_firmware() {
+    if [ "${SRC}" != "adb" ]; then
+        local STAR="${ANDROID_ROOT}"/lineage/scripts/motorola/star.sh
+        for IMAGE in bootloader radio; do
+            if [ -f "${SRC}/${IMAGE}.img" ]; then
+                echo "Extracting Motorola star image ${SRC}/${IMAGE}.img"
+                sh "${STAR}" "${SRC}/${IMAGE}.img" "${SRC}"
+            fi
+        done
+        local INFO="${ANDROID_ROOT}"/lineage/scripts/motorola/info.sh
+        ./${INFO} "${SRC}"
+    fi
+}
+
+if [ -z "${ONLY_FIRMWARE}" ] && [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    # Extract common proprietary files
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-    extract "${MY_DIR}/proprietary-files-fm.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-    extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
-if [[ -z "${ONLY_COMMON}" ]] && [[ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]]; then
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../../${VENDOR}/${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    # Extract device-specific proprietary files
-    extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    if [ -z "${ONLY_FIRMWARE}" ]; then
+        extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    fi
+
+    if [ -z "${SECTION}" ] && [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
+        extract_firmware "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" "${SRC}"
+    fi
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
