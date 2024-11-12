58ab128ed33280149f0a11ce80d0b6c50f64308d
Rakhshan Ali <121305132+Rakhshan7070@users.noreply.github.com>
2024-11-12 18:46:03 +0530

    sm6375-common: Switch to AIDL thermal HAL

--- common.mk
+++ common.mk
@@ -406,7 +406,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti \
+    android.hardware.thermal-service.qti \
     android.hardware.thermal@2.0 \
     android.hardware.thermal@2.0.vendor
 
