904e00253691522b15532e7a4ef59df6868e22c3
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-03-01 00:23:34 +0530

    sm6375-common: Nuke duplicate powerhint declaration

--- common.mk
+++ common.mk
@@ -290,9 +290,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
-PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 
